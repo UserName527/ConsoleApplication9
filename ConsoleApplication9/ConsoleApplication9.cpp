77dc1286b29375ce4e78b54bd797c17a759cd8cd
Atos <70628905+ireadyium@users.noreply.github.com>
2023-10-06 09:14:45 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "Введите a,b,x:";
+    float a, b, x;
+    std::cin >> a;
+    std::cin >> b;
+    std::cin >> x;
+    float result = pow((pow(x, 3) + pow(a, 3) + pow(b, 3)),1/3);
+    std::cout << result;
+}
